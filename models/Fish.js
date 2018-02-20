f74132608bf705ae76f52781fe24e8bbff7d2b9c
Micael Persson <micael.c.persson@gmail.com>
2018-02-20 15:34:03 +0100

    Made improvements to Fish-model.

--- models/Fish.js
+++ models/Fish.js
@@ -4,12 +4,34 @@ mongoose.Promise = global.Promise;
 
 const fishSchema = new mongoose.Schema({
   user: String,
-  position: [Number],
-  species: String,
-  weight: Number,
-  length: Number,
+  species: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name',
+  },
+  weight: {
+    type: Number,
+    required: 'Please enter a store name',
+  },
+  length: {
+    type: Number,
+    required: 'Please enter a store name',
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point',
+    },
+    coordinates: [{
+      type: Number,
+      required: 'You must supply coodinates!',
+    }],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
   imageUrl: String,
-  description: String,
   tags: [String],
 });
 
